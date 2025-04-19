fc8547312f4905f713060d31bf7e837defacf53a
Ploypaphat Saltz <ploypaphat.saltz@gmail.com>
2025-04-19 13:31:40 -0700

    [refactor]: consolidate Foodie and Eatery by making them share common attributes via User

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,28 +1,41 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# Create the Base class
 Base = declarative_base()
 
-class Foodie(Base):
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    tag = Column(String, unique=True, index=True, nullable=True)
+    register_date = Column(String, nullable=False)
+    end_date = Column(String, nullable=True)
+    password = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'user',
+        'polymorphic_on': type,         
+    }
+
+class Foodie(User):
     __tablename__ = 'foodie'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     phone = Column(String, unique=True, index=True, nullable=True)
-    tag = Column(String, unique=True, index=True, nullable=True)
-    register_date = Column(String, nullable=False)
-    end_date = Column(String, nullable=True)
-    password = Column(String, nullable=False)
-    
-class Eatery(Base):
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'foodie',
+    }
+
+class Eatery(User):
     __tablename__ = 'eatery'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    tag = Column(String, unique=True, index=True, nullable=True)
-    register_date = Column(String, nullable=False)
-    end_date = Column(String, nullable=True)
-    password = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True)
+    website = Column(String, unique=True, index=True, nullable=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'eatery', 
+    }
